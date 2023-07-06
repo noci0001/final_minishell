f8045195d2b17ae947ef0cbe704fad9a91867403
Sam <noci0001@hz.nl>
2023-07-06 13:16:14 +0200

    cd oh my cd

--- src/builtins/ft_cd.c
+++ src/builtins/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: snocita <samuelnocita@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 15:43:04 by snocita           #+#    #+#             */
-/*   Updated: 2023/07/06 12:44:26 by snocita          ###   ########.fr       */
+/*   Updated: 2023/07/06 13:15:28 by snocita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,14 @@
 int	ft_cd(t_cmd	*cmd, t_env	*env)
 {
 	char	*home;
+	//char	*before_path;
 
-	(void)cmd;
+	//(void)cmd;
 	home = ft_get_env(env, "HOME");
 	printf("HOME is -> %s\n", home);
+	// if (is_arg_absent(cmd, env, 0) == 1)
+	// {
+	// 	if(chdir(home) == 1)
+	// }
 	return (1);
 }
--- src/builtins/ft_pwd.c
+++ src/builtins/ft_pwd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_pwd.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: snocita <snocita@student.42wolfsburg.de>   +#+  +:+       +#+        */
+/*   By: snocita <samuelnocita@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 18:30:59 by snocita           #+#    #+#             */
-/*   Updated: 2023/07/02 15:20:02 by snocita          ###   ########.fr       */
+/*   Updated: 2023/07/06 13:04:01 by snocita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	ft_pwd(t_env	*env)
 {
 	char	*pwd;
 
-	pwd = ft_get_env(env, "PWD=");
+	pwd = ft_get_env(env, "PWD");
 	ft_putendl_fd(pwd + 5, 1);
 	return (1);
 }
